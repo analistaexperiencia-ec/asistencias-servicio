746e4ecbd642742d591be042d87fa1c8483a652b
analistaexperiencia-ec <analista.experiencia@gmail.com>
2026-01-30 17:05:13 -0500

    new

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': process.env
-  },
-  server: {
-    port: 3000
+    // Definimos process.env para que el código del frontend pueda acceder a la API_KEY inyectada por Netlify
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY || '')
   },
   build: {
     outDir: 'dist',
-    sourcemap: true
+    emptyOutDir: true,
+    target: 'esnext'
+  },
+  server: {
+    port: 3000
   }
 });
